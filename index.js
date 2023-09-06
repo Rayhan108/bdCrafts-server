ab25244a15c1b071ea0e11d61222aaf6523bcb79
Rayhan Shorker <raihanshorker56@gmail.com>
2023-09-06 13:18:28 +0600

    fixed bug

--- index.js
+++ index.js
@@ -304,8 +304,11 @@ async function run() {
     // delete products
     app.delete("/deleteProducts/:id", async (req, res) => {
       const id = req.params.id;
+      
       const query = { _id: new ObjectId(id) };
+      
       const result = await productsCollection.deleteOne(query);
+      
       res.send(result);
     });
 
